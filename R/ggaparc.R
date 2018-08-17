e05850141ffabb27653920818cd8e689a24c9feb
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-08-17 16:54:00 +0200

    ggaparc with two hemispheres, and medial view

--- R/ggaparc.R
+++ R/ggaparc.R
@@ -9,15 +9,19 @@
 #'
 #' @param plot.areas Character vector, plots only areas specified in the vector.
 #' @param mapping ggplot2 aethetics (cannot include x and y aethetics)
-#' @param na.fill string or HEX code for the fill of the area without values
-#' @param colour string or HEX code for the colour of the outlines of each area
+#' @param hemisphere String to choose hemisphere to plot. Any of c("lh","rh")[default].
+#' @param na.fill String or HEX code for the fill of the area without values
+#' @param colour String or HEX code for the colour of the outlines of each area
 #' @param size Numeric, size of the line outlining each area
 #' @param show.legend logical, toggle on or off legend.
 #' @param ...
 #'
 #' @return a ggplot object
 #'
+#' @importFrom dplyr filter left_join
 #' @importFrom ggplot2 ggplot aes geom_polygon coord_fixed
+#' @importFrom stats na.omit
+#' @importFrom tidyr separate
 #'
 #' @examples
 #' ggaparc()
@@ -27,39 +31,56 @@
 #' @seealso [ggplot()], [aes()], [geom_polygon()], [coord_fixed()] from the ggplot2 package
 #'
 #' @export
-ggaparc = function(data = NULL, plot.areas=NULL, mapping = NULL,
+ggaparc = function(data = NULL, plot.areas=NULL,
+                   hemisphere = c("rh","lh"),
+                   mapping = NULL, alpha = .9,
                    colour="white", size=.3, show.legend = NA,
                    na.fill="grey",...){
 
   fileLoc = system.file("data","geobrain", package = "MOAS")
   nn = load(paste0(fileLoc,"/geobrain.bilateral.Rda"))
-  geobrain = get(nn) %>% separate(aparc, c("hemisphere","area"),remove = F,convert = T)
+  geobrain = get(nn) %>%
+    tidyr::separate(aparc, c("hemi","area"),remove = F,convert = T) %>%
+    dplyr::filter(hemi %in% hemisphere) %>%
+    dplyr::mutate(aparc=ifelse(grepl("medialwall",aparc),NA,aparc),
+                  area=ifelse(grepl("medialwall",area),NA,area))
+
+
 
   if(!is.null(plot.areas)){
     if(any(!plot.areas %in% geobrain$area)){
       stop(paste("There is no", plot.areas, "in aparc data. Check spelling. Options are:",paste0(geobrain$area %>% unique,collapse=", ")))
     }
-    geobrain = geobrain %>% filter(area %in% plot.areas)
+    geobrain = geobrain %>% dplyr::filter(area %in% plot.areas)
   }
 
-  geoData = if(is.null(data)){
-    geobrain
+  geoData = if(!is.null(data)){
+     geobrain %>%
+      dplyr::left_join(data) %>%
+      stats::na.omit()
   }else{
-    geobrain %>% dplyr::left_join(data, by="area") %>% na.omit()
+    geobrain
+  }
+
+  if(is.null(mapping)){
+    mapping=ggplot2::aes(alpha=.0)
+    show.legend=F
   }
 
   ggplot2::ggplot(data = geobrain, ggplot2::aes(x=long, y=lat, group=id)) +
     ggplot2::geom_polygon(
       size=size,
       colour=colour,
-      fill=na.fill) +
+      fill=na.fill,
+      alpha=alpha) +
     ggplot2::geom_polygon(
       data=geoData,
       mapping=mapping,
       size=size,
       colour=colour,
-      show.legend = show.legend) +
-    ggplot2::coord_fixed()
+      show.legend = show.legend)+
+      ggplot2::coord_fixed()
+
 }
 
 
