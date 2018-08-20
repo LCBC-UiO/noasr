3000968acca02fce07e1f0091b6564ee35b2233b
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-08-20 08:49:22 +0200

    change ggaparc to ggbrain, added atlas and side view options

--- R/ggaparc.R
+++ R/ggaparc.R
@@ -1,4 +1,4 @@
-#' Plot Freesurfer aparc areas
+#' Plot brain parcellations
 #'
 #' \code{ggaparc} plots and returns a ggplot object of plotted
 #' aparc areas.
@@ -31,20 +31,21 @@
 #' @seealso [ggplot()], [aes()], [geom_polygon()], [coord_fixed()] from the ggplot2 package
 #'
 #' @export
-ggaparc = function(data = NULL, plot.areas=NULL,
+ggbrain = function(data = NULL, plot.areas=NULL,
                    position="dispersed",
-                   hemisphere = c("rh","lh"),
+                   atlas="DKT", view=c("lateral","medial"),
+                   hemisphere = c("right","left"),
                    mapping = NULL, alpha=NA,
                    colour="white", size=.3, show.legend = NA,
                    na.fill="grey",...){
 
+  # Load the segmentation to use
   fileLoc = system.file("data","geobrain", package = "MOAS")
-  nn = load(paste0(fileLoc,"/geobrain.bilateral.Rda"))
+  file=list.files(fileLoc, pattern=atlas,full.names = T)
+  nn = load(file)
   geobrain = get(nn) %>%
-    tidyr::separate(aparc, c("hemi","area"),remove = F,convert = T) %>%
-    dplyr::filter(hemi %in% hemisphere) %>%
-    dplyr::mutate(aparc=ifelse(grepl("medialwall",aparc),NA,aparc),
-                  area=ifelse(grepl("medialwall",area),NA,area))
+    dplyr::filter(hemi %in% hemisphere &
+                    side %in% view)
 
   if(position=="stacked"){
     stack = geobrain %>%
@@ -60,21 +61,15 @@ ggaparc = function(data = NULL, plot.areas=NULL,
       )
   }
 
-
+  # Filter data to single area if that is all you want.
   if(!is.null(plot.areas)){
     if(any(!plot.areas %in% geobrain$area)){
-      stop(paste("There is no", plot.areas, "in aparc data. Check spelling. Options are:",paste0(geobrain$area %>% unique,collapse=", ")))
+      stop(paste("There is no", plot.areas,
+                 "in", atlas,"data. Check spelling. Options are:",
+                 paste0(geobrain$area %>% unique,collapse=", ")))
     }
     geobrain = geobrain %>% dplyr::filter(area %in% plot.areas)
   }
-
-  geoData = if(!is.null(data)){
-    geobrain %>%
-      dplyr::left_join(data)
-  }else{
-    geobrain
-  } %>%
-    stats::na.omit()
 
 
   gg = ggplot2::ggplot(data = geobrain, ggplot2::aes(x=long, y=lat, group=id)) +
@@ -82,20 +77,48 @@ ggaparc = function(data = NULL, plot.areas=NULL,
       size=size,
       colour=colour,
       fill=na.fill,
-      alpha=alpha)
+      alpha=alpha) +
+    ggplot2::coord_fixed()
 
   if(!is.null(mapping)){
+
+    geoData = geobrain
+    if(!is.null(data))
+      geoData = geoData %>%
+        dplyr::left_join(data)
+
     gg = gg +
       ggplot2::geom_polygon(
-        data=geoData,
+        data=geoData %>% stats::na.omit(),
         mapping=mapping,
         size=size,
         colour=colour,
         show.legend = show.legend)
   }
 
-  gg + ggplot2::coord_fixed()
+  if(position == "stacked"){
+    gg = gg +
+      scale_y_continuous(
+        breaks=c(2,8),
+        labels=c("Right","Left")) +
+      scale_x_continuous(breaks=NULL) +
+      labs(x=NULL, y="Hemisphere")
+  }else{
+   gg = gg +
+     scale_x_continuous(
+     breaks=c(8,25.3),
+     labels=c("Right","Left")) +
+     scale_y_continuous(breaks=NULL)+
+     labs(y=NULL, x="Hemisphere")
+  }
 
+  gg +
+    theme(
+      axis.ticks = element_blank(),
+      panel.grid = element_blank(),
+      panel.background = element_blank(),
+      plot.background = element_blank()
+    )
 }
 
 
