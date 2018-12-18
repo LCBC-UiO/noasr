9dde9843fdc8a96674611e5dd7a131453584b9ff
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-12-18 17:28:55 +0100

    fixed shiny error

--- inst/shiny/LCBCshiny/global.R
+++ inst/shiny/LCBCshiny/global.R
@@ -5,6 +5,7 @@ library(tidyverse);
 #Source all the necessary functions
 lapply(list.files("subscripts/", pattern=".R$",full.names = T), source)
 
+lapply(list.files("globalVars/", pattern=".RData$",full.names = T), load, envir=.GlobalEnv)
 
 ConversionTab = rio::import("Documentation/Project_Harmonization.xlsx") %>%
    dplyr::select(1:5)
