b44172165fb1a14ec95211a1801550c52819f4c5
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2019-08-21 09:04:49 +0200

    added funcs and vignette for project documentation

--- R/docs-paradigm.R
+++ R/docs-paradigm.R
@@ -5,6 +5,7 @@
 #' @param path path to Paradigm documentation
 #' @param bib logical, initiate bibTex file or not
 #' @export
+#' @importFrom utils file.edit
 docs_paradigm_page <- function(name = NULL, type = NULL, path = ".", bib=TRUE){
 
   types <- c("rt", "sp", "fmri", "erp")
@@ -29,14 +30,14 @@ docs_paradigm_page <- function(name = NULL, type = NULL, path = ".", bib=TRUE){
                                 "skeleton.bib", package = "MOAS")
     out <- paste0("bibTex/", filename, '.bib')
     file.copy(template_bib, out)
-    file.edit(out)
+    utils::file.edit(out)
   }
 
   template_rmd <- system.file("rmarkdown/templates/lcbc-paradigm-page/skeleton",
                               "skeleton.Rmd", package = "MOAS")
   out <- paste0(path, "/", sprintf("%02d", cnt), "-", filename, '.Rmd')
   file.copy(template_rmd, out)
-  file.edit(out)
+  utils::file.edit(out)
 }
 
 
@@ -50,6 +51,10 @@ docs_paradigm_page <- function(name = NULL, type = NULL, path = ".", bib=TRUE){
 #' @param path path to Paradigm_documentation folder
 #'
 #' @export
+#' @importFrom dplyr filter mutate arrange
+#' @importFrom purrr walk2
+#' @importFrom tidyr unite
+#' @importFrom magrittr '%>%'
 docs_paradigm_order_chapter <- function(path = "."){
 
   types <- c("rt", "sp", "fmri", "erp")
@@ -57,21 +62,33 @@ docs_paradigm_order_chapter <- function(path = "."){
   # Find existing chapters
   chapters <- data.frame(orig = list.files(path, "Rmd"),
                          stringsAsFactors = FALSE) %>%
-    filter(grepl(paste0("-", types, collapse="|"), orig)) %>%
-    mutate(raw = gsub("[[:digit:]]", "", orig),
+    dplyr::filter(grepl(paste0("-", types, collapse="|"), orig)) %>%
+    dplyr::mutate(raw = rm_chapter_num(orig),
            type = get_type(orig)) %>%
-    mutate(type = factor(type, levels = types)) %>%
-    arrange(type, raw) %>%
-    mutate(num = sprintf("%02d", seq_along(raw))) %>%
-    unite(new, c(num, raw), sep="")
+    dplyr::mutate(type = factor(type, levels = types)) %>%
+    dplyr::arrange(type, raw) %>%
+    dplyr::mutate(num = sprintf("%02d", seq_along(raw))) %>%
+    tidyr::unite(new, c(num, raw), sep="")
 
   purrr::walk2(chapters$orig, chapters$new, ~ file.rename(.x, .y))
 }
 
 get_type <- function(string){
-  t <- character()
-  for(i in 1:length(string)){
-    t[i] <- strsplit(string[i], "-")[[1]][2]
-  }
-  t
+
+  t <- sapply(string,
+              function(x) strsplit(x, "-")[[1]][2]
+  )
+
+  unname(t)
+}
+
+rm_chapter_num <- function(string){
+
+  t <- sapply(string,
+         function(x) paste0("-",
+         strsplit(x, "-")[[1]][-1],
+         collapse = "")
+  )
+
+  unname(t)
 }
